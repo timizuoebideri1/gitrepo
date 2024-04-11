6287f85dabd7a2fef67b349cb6471722df1d8c1c
Timizuo <94907097+timizuoebideri1@users.noreply.github.com>
2024-04-11 07:42:58 +0200

    Update job_module.py

--- jobs/job_module.py
+++ jobs/job_module.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import uuid
 
 from django.conf import settings
 from django.db import transaction
@@ -16,65 +17,49 @@ from nautobot.apps.jobs import (
 )
 from nautobot.dcim.models import Device, Location
 from nautobot.extras.choices import ObjectChangeActionChoices
+from nautobot.extras.models import Tag
 
-name = "Git Repo Job"
+name = "Dean Run Jobs"
 
 
-class GitRepoJob(Job):
+class AddNewTagsJob(Job):
     dryrun = DryRunVar()
 
     class Meta:
-        name = "Git Repo Dry Run"
-        approval_required = True
-        has_sensitive_variables = False
-        description = "Example job to remove serial number on all devices, supports dryrun mode."
+        name = "Add New Tags Job"
+        description = "Add new Tags."
 
     def run(self, dryrun):
         try:
             with transaction.atomic():
-                devices_with_serial = Device.objects.exclude(serial="")
-                log_msg = "Removing serial on %s devices."
+                tags_to_create = Tag(name=f"Tag From Job {uuid.uuid4().hex[:5]}")
+                log_msg = "Creating tag %s."
                 if dryrun:
                     log_msg += " (DRYRUN)"
-                self.logger.info(log_msg, devices_with_serial.count())
-                for device in devices_with_serial:
-                    if not dryrun:
-                        device.serial = ""
-                        device.save()
+                self.logger.info(log_msg, tags_to_create.name)
+                if not dryrun:
+                    tags_to_create.save()
         except Exception:
             self.logger.error("%s failed. Database changes rolled back.", self.__name__)
             raise
 
-class ExampleJobHookReceiver(JobHookReceiver):
+
+
+class AddNewTagsJobHookReceiver(JobHookReceiver):
     class Meta:
-        name = "Example job hook receiver"
+        name = "Add new Tag job hook receiver"
         description = "Validate changes to object serial field"
 
     def receive_job_hook(self, change, action, changed_object):
         # return on delete action
         if action == ObjectChangeActionChoices.ACTION_DELETE:
             return
+        
+        last_tag = Tag.objects.last()
+        if not last_tag or not last_tag.name.startswith("Tag From Job"):
+            self.logger.warning("No Tags was created from jobs")
+        else:
+            last_tag.name = last_tag.name + " (HOOK PASSED)"
+            self.logger.info("Last Tag job created successfully has passed check")
 
-        # log diff output
-        snapshots = change.get_snapshots()
-        self.logger.info("DIFF: %s", snapshots["differences"])
-
-        # validate changes to serial field
-        if "serial" in snapshots["differences"]["added"]:
-            old_serial = snapshots["differences"]["removed"]["serial"]
-            new_serial = snapshots["differences"]["added"]["serial"]
-            self.logger.info("%s serial has been changed from %s to %s", changed_object, old_serial, new_serial)
-
-            # Check the new serial is valid and revert if necessary
-            if not self.validate_serial(new_serial):
-                changed_object.serial = old_serial
-                changed_object.save()
-                self.logger.info("%s serial %s was not valid. Reverted to %s", changed_object, new_serial, old_serial)
-
-            self.logger.info("Serial validation completed for %s", changed_object)
-
-    def validate_serial(self, serial):
-        # add business logic to validate serial
-        return False
-
-register_jobs(GitRepoJob, ExampleJobHookReceiver)
+register_jobs(AddNewTagsJob, AddNewTagsJobHookReceiver)
