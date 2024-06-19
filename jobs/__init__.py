d4ca9ccc85cd87c15454ea25c6c5d488c61356a2
timizuoebideri <timizuoebideri@gmail.com>
2024-06-19 16:41:04 +0200

    okay

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,5 +1,25 @@
 from nautobot.core.celery import register_jobs
-from .job_module import (GitRepoTrialJob)
+# from .job_module import (GitRepoTrialJob)
+from nautobot.extras.jobs import Job
 
-# register_jobs(GitRepoTrialJob)
-print("XXXXXXXXXXXX IM SKIPPING REG TODAY XXXXXXXXXXXXXXX")
+class GitRepoTrialJob(Job):
+    class Meta:
+        name = "Git Repo Trial Jobs 4"
+        description = """
+            Markdown Formatting
+
+            *This is italicized*
+        """
+        # soft_time_limit = 15 * 60
+    
+    def run(self):
+        # self.logger.debug("Running for %s seconds.", interval)
+        self.logger.info("Step %s", 4)
+        import time
+        for i in range(10):
+            time.sleep(60)
+            self.logger.info(f"I have slept for {i} min")
+        self.logger.info("Im done sleeping")
+        
+register_jobs(GitRepoTrialJob)
+# print("XXXXXXXXXXXX IM SKIPPING REG TODAY XXXXXXXXXXXXXXX")
