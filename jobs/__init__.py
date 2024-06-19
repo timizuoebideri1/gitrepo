7c83627d82125e6ea9b9368fdd281bca820d936e
timizuoebideri <timizuoebideri@gmail.com>
2024-06-19 16:30:52 +0200

    okay

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,5 @@
 from nautobot.core.celery import register_jobs
 from .job_module import (GitRepoTrialJob)
 
-register_jobs(GitRepoTrialJob)
+# register_jobs(GitRepoTrialJob)
+print("XXXXXXXXXXXX IM SKIPPING REG TODAY XXXXXXXXXXXXXXX")
