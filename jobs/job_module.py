fcf5895daf0846b283d61233151626d8860e2fa6
timizuoebideri <timizuoebideri@gmail.com>
2024-06-20 19:26:57 +0200

    ok

--- jobs/job_module.py
+++ jobs/job_module.py
@@ -3,7 +3,7 @@ from nautobot.extras.jobs import Job
 
 class GitRepoTrialJob(Job):
     class Meta:
-        name = "Git Repo Trial New Name 3"
+        name = "Git Repo Trial"
         description = """
             Markdown Formatting
 
