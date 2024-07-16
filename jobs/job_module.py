ab7f0644ed848901ae3bded2663811d4f3ec623f
timizuoebideri <timizuoebideri@gmail.com>
2024-07-16 10:42:46 +0200

    good

--- jobs/job_module.py
+++ jobs/job_module.py
@@ -9,13 +9,14 @@ class GitRepoTrialJob(Job):
 
             *This is italicized*
         """
-        # soft_time_limit = 15 * 60
+        soft_time_limit = 1 * 60
+        time_limit = 2 * 60
     
     def run(self):
-        # self.logger.debug("Running for %s seconds.", interval)
+        self.logger.debug("Running for seconds.")
         self.logger.info("Step %s", 4)
         import time
         for i in range(10):
-            time.sleep(60)
-            self.logger.info(f"I have slept for {i} min")
+            time.sleep(10)
+            self.logger.info(f"I have slept for {i + 10} sec")
         self.logger.info("Im done sleeping")
